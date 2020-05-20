862a6392fa355b12a5a70265a597d9ae4f43e055
wu <422603251@qq.com>
2020-05-20 14:08:54 +0800

    整理目录等

---
+++ readme.txt
@@ -0,0 +1,11 @@
+编译步骤：
+1、安装cmake3
+yum install cmake3
+
+2、build下执行cmake3
+cmake3 ../
+
+3、build下执行make
+make
+
+4、生成的目标文件在bin下
--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VERSION "2020-05-19.1445"
+#define VERSION "medical 20-05-19.1445"
 
 #endif // VERSION_H
